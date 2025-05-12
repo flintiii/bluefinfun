433f76389c256bfc1ba15922050f7b1487c7aeb9
Flint <flint@flint.com>
2025-05-12 18:00:44 -0400

    add x3270

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux pluma gparted micropython screen
+dnf5 install -y tmux pluma gparted micropython screen x3270
 
 # Use a COPR Example:
 #
